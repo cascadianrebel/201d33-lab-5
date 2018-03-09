d4b92acafd350c63e13829a98ec9014abe551e62
Anthony Green <Anthonydgreen90@gmail.com>
2018-03-09 15:40:10 -0800

    solved probelem 3

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   var productTwoNums = a*b;
   var productString = 'The product of ' + a + ' and ' + b + ' is ' + productTwoNums + '.';
-  return [productTwoNums, productString]
+  return [productTwoNums, productString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -55,11 +55,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumThreeNum = sum(sum(a,b)[0],c)[0];
+  var productThreeNum = multiply(multiply(a,b)[0],c)[0];
+  var sumThreeString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumThreeNum + '.';
+  var productThreeString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productThreeNum + '.';
+  return [sumThreeNum, productThreeNum, sumThreeString, productThreeString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
